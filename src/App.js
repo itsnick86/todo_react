66358a54cc07886e52ed7dc87125fb4554bc3795
Nick Edwards <nledwa01@gmail.com>
2023-03-07 17:39:48 -0600

    Updated To Do Grid to loop over array to remove hard coded items.

--- src/App.js
+++ src/App.js
@@ -4,11 +4,17 @@ import ToDoGrid from './components/ToDoGrid';
 import './App.css';
 
 function App() {
+    const initToDoList = [
+        { id: 1, title: 'Build Wireframe', completed: false },
+        { id: 2, title: 'Map Out Components', completed: false },
+        { id: 3, title: 'Build The App', completed: false },
+    ];
+
     return (
         <div className="container">
             <h1 className="text-center">Things To Do</h1>
             <Filter />
-            <ToDoGrid />
+            <ToDoGrid toDoItems={initToDoList} />
         </div>
     );
 }
--- src/components/ToDoGrid.js
+++ src/components/ToDoGrid.js
@@ -10,78 +10,39 @@ const ToDoGrid = props => {
                     aria-describedby="ToDoInput"
                 ></input>
             </div>
-            <div className="row border">
-                <div className="form-check col-auto me-auto">
-                    <input
-                        className="form-check-input"
-                        type="checkbox"
-                        value=""
-                        id="completeToDoCheckbox"
-                    />
-                    <label
-                        className="form-check-label"
-                        htmlFor="completeToDoCheckbox"
-                    >
-                        Build Wireframe
-                    </label>
-                </div>
-                <div className="col-auto">
-                    <button type="button" className="btn btn-outline-warning">
-                        Edit
-                    </button>
-                    <button type="button" className="btn btn-outline-danger">
-                        Delete
-                    </button>
-                </div>
-            </div>
-            <div className="row border">
-                <div className="form-check col-auto me-auto">
-                    <input
-                        className="form-check-input"
-                        type="checkbox"
-                        value=""
-                        id="completeToDoCheckbox"
-                    />
-                    <label
-                        className="form-check-label"
-                        htmlFor="completeToDoCheckbox"
-                    >
-                        Map Out Components
-                    </label>
-                </div>
-                <div className="col-auto">
-                    <button type="button" className="btn btn-outline-warning">
-                        Edit
-                    </button>
-                    <button type="button" className="btn btn-outline-danger">
-                        Delete
-                    </button>
-                </div>
-            </div>
-            <div className="row border">
-                <div className="form-check col-auto me-auto">
-                    <input
-                        className="form-check-input"
-                        type="checkbox"
-                        value=""
-                        id="completeToDoCheckbox"
-                    />
-                    <label
-                        className="form-check-label"
-                        htmlFor="completeToDoCheckbox"
-                    >
-                        Build The App
-                    </label>
-                </div>
-                <div className="col-auto">
-                    <button type="button" className="btn btn-outline-warning">
-                        Edit
-                    </button>
-                    <button type="button" className="btn btn-outline-danger">
-                        Delete
-                    </button>
-                </div>
-            </div>
+            {props.toDoItems.map(toDoItems => {
+                return (
+                    <div key={toDoItems.id} className="row border">
+                        <div className="form-check col-auto me-auto">
+                            <input
+                                className="form-check-input"
+                                type="checkbox"
+                                value=""
+                                id="completeToDoCheckbox"
+                            />
+                            <label
+                                className="form-check-label"
+                                htmlFor="completeToDoCheckbox"
+                            ></label>
+                            {toDoItems.title}
+                        </div>
+                        <div className="col-auto">
+                            <button
+                                type="button"
+                                className="btn btn-outline-warning"
+                            >
+                                Edit
+                            </button>
+                            <button
+                                type="button"
+                                className="btn btn-outline-danger"
+                            >
+                                Delete
+                            </button>
+                        </div>
+                    </div>
+                );
+            })}
         </div>
     );
 };
